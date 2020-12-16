423d2cb400371b113586e1e3e20acb6939b352a3
Ariel Duek <arielduek@gmail.com>
2020-12-16 22:08:12 +0200

    fix auto movement

--- Assets/Scripts/0-bfs/TilemapGraph.cs
+++ Assets/Scripts/0-bfs/TilemapGraph.cs
@@ -17,6 +17,56 @@ public class TilemapGraph : IGraph<Vector3Int>
         this.allowedTiles = allowedTiles;
     }
 
+    static Vector3Int
+        LEFT = new Vector3Int(-1, 0, 0),
+        RIGHT = new Vector3Int(1, 0, 0),
+        DOWN = new Vector3Int(0, -1, 0),
+        DOWNLEFT = new Vector3Int(-1, -1, 0),
+        DOWNRIGHT = new Vector3Int(1, -1, 0),
+        UP = new Vector3Int(0, 1, 0),
+        UPLEFT = new Vector3Int(-1, 1, 0),
+        UPRIGHT = new Vector3Int(1, 1, 0);
+
+    static Vector3Int[] directions_when_y_is_even = { LEFT, RIGHT, DOWN, DOWNLEFT, UP, UPLEFT };
+    static Vector3Int[] directions_when_y_is_odd = { LEFT, RIGHT, DOWN, DOWNRIGHT, UP, UPRIGHT };
+
+    public IEnumerable<Vector3Int> Neighbors(Vector3Int node)
+    {
+        var directions = (node.y % 2 == 0 ? directions_when_y_is_even : directions_when_y_is_odd);
+        foreach (var direction in directions)
+        {
+            Vector3Int neighborPos = node + direction;
+            TileBase neighborTile = tilemap.GetTile(neighborPos);
+            Debug.Log(node + " --> " + neighborPos);
+            if (allowedTiles.Contains(neighborTile))
+            {
+                yield return neighborPos;
+            }
+        }
+    }
+}
+
+
+/**
+using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+/**
+ * A graph that represents a tilemap, using only the allowed tiles.
+ /
+public class TilemapGraph : IGraph<Vector3Int>
+{
+    private Tilemap tilemap;
+    private TileBase[] allowedTiles;
+
+    public TilemapGraph(Tilemap tilemap, TileBase[] allowedTiles)
+    {
+        this.tilemap = tilemap;
+        this.allowedTiles = allowedTiles;
+    }
+
     static Vector3Int[] directions = {
             new Vector3Int(-1, 1, 0),
             new Vector3Int(0, 1, 0),
@@ -37,3 +87,4 @@ public class TilemapGraph : IGraph<Vector3Int>
         }
     }
 }
+**/
