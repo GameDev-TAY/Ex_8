2949452c80e896209ef69d2049e43f89213e0776
Ariel Duek <arielduek@gmail.com>
2020-12-16 13:51:38 +0200

    Change Map And Keys on Keyboard movement

--- Assets/Scripts/2-player/KeyboardMover.cs
+++ Assets/Scripts/2-player/KeyboardMover.cs
@@ -6,15 +6,20 @@
 public class KeyboardMover : MonoBehaviour {
 
     protected Vector3 NewPosition() {
-        if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+        if (Input.GetKeyDown(KeyCode.F)) {
             return transform.position + Vector3.left;
-        } else if (Input.GetKeyDown(KeyCode.RightArrow)) {
+        } else if (Input.GetKeyDown(KeyCode.H)) {
             return transform.position + Vector3.right;
-        } else if (Input.GetKeyDown(KeyCode.DownArrow)) {
+        } else if (Input.GetKeyDown(KeyCode.V)) {
             return transform.position + new Vector3(-0.5f, -0.75f);
-        } else if (Input.GetKeyDown(KeyCode.UpArrow)) {
+        } else if (Input.GetKeyDown(KeyCode.Y)) {
             return transform.position + new Vector3(0.5f, 0.75f);
-        } else {
+        }else if (Input.GetKeyDown(KeyCode.T)) {
+            return transform.position + new Vector3(-0.5f, 0.75f);
+        }else if (Input.GetKeyDown(KeyCode.B)) {
+            return transform.position + new Vector3(0.5f, -0.75f);
+        }
+        else {
             return transform.position;
         }
     }
